ac0995d2b118ac34864876c3fb62f81874778c7a
James Oladimeji <elderjames314@gmail.com>
2023-07-01 05:48:22 +0100

    Project initialization

---
+++ src/main/java/com/blusalt/dronemgtsystem/DroneMgtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.blusalt.dronemgtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneMgtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneMgtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blusalt/dronemgtsystem/DroneMgtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blusalt.dronemgtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneMgtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
